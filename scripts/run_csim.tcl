b75518239dc62715bbefea88fbfe644715d4d3b0
ShahabuddinDanish <shahabuddin.danish7@gmail.com>
2025-08-29 23:02:16 +0200

    fix: correct tb filename in csim tcl script.

--- scripts/run_csim.tcl
+++ scripts/run_csim.tcl
@@ -5,7 +5,7 @@ open_project -reset "subiso_csim_proj"
 add_files "./source/logger.cpp"
 add_files "./source/cmdlineparser.cpp"
 add_files "./source/subgraphIsomorphism.cpp" -cflags "-std=c++17"
-add_files -tb "./source/tb-subiso.cpp"
+add_files -tb "./source/tb_csim.cpp"
 add_files -tb "./dataset_example"
 add_files -tb "./scripts"
 set_top subgraphIsomorphism
